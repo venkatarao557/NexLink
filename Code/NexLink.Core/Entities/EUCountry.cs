e77d7e18823c57e979a424f20ee5b7a4e3e9906c
Venkat <venkatarao.p@gmail.com>
2026-04-16 18:33:31 +1000

    Adding back EUCountry.cs with proper case as in the code

---
+++ Code/NexLink.Core/Entities/EUCountry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace NexLink.Core.Entities;
+
+[Table("EUCountry")]
+[Index("EuCountryCode", Name = "UQ_EUCountry_Code", IsUnique = true)]
+public partial class EUCountry
+{
+    [Key]
+    [Column("EUCountryID")]
+    public Guid EuCountryId { get; set; }
+
+    [Column("EUCountryCode")]
+    [StringLength(5)]
+    public string EuCountryCode { get; set; } = null!;
+
+    [Column("EUCountryName")]
+    [StringLength(100)]
+    public string EuCountryName { get; set; } = null!;
+}
